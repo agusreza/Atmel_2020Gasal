d4887a056e6d08e9b3ff37fa88318f2f8d2bb46d
agusreza <agus.reza@gmail.com>
2020-11-13 15:54:28 +0700

    Project Seven Segment
    menambahkan encoding bit 7-segment hasil karya bersama

--- Trial_portOutSeven/Trial_portOutSeven/main.c
+++ Trial_portOutSeven/Trial_portOutSeven/main.c
@@ -9,17 +9,60 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void kirimkeDisplay (uint8_t angka);
+
+void kirimkeDisplay (uint8_t angka);	// subroutine / subbagian
 
 int main(void)
 {
+	uint8_t counter;
     
 	DDRD = 0xFF;
 	PORTD = 0xff;
-	
+
+	counter = 0;
+		
     while (1) 
     {
+		kirimkeDisplay(counter);
+		counter++;
+		_delay_ms(1000);
+		
 		
     }
+}
+
+void kirimkeDisplay (uint8_t angka)
+{
+	uint8_t	dataSevenOut;		//local variable
+	
+	dataSevenOut = 0x00;
+	
+	switch (angka)
+	{
+	case 0: dataSevenOut = 0b00111111;
+		break;
+	case 1: dataSevenOut = 0b00000110;
+		break;
+	case 2: dataSevenOut = 0b01011011;
+		break;
+	case 3: dataSevenOut = 0b01001111;
+		break;
+	case 4: dataSevenOut = 0b01100110;
+		break;
+	case 5: dataSevenOut = 0b01101101;
+		break;
+	case 6: dataSevenOut = 0b01111101;
+		break;
+	case 7: dataSevenOut = 0b00000111;
+		break;
+	case 8: dataSevenOut = 0b00111111;
+		break;
+	case 9: dataSevenOut = 0b11101111;
+		break;
+	default: 
+		break;
+	}
+	
+	PORTD = dataSevenOut;
 }
 
